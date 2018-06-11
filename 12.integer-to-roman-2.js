e348391b4cc096808239d9ff453ae639de506860
Pavlo Genov <paulgenov@gmail.com>
2018-06-11 14:58:37 +0300

    12. Integer to Roman. Faster Solution

---
+++ 12.integer-to-roman-2.js
@@ -0,0 +1,53 @@
+/**
+ * Given an integer, convert it to a roman numeral.
+ * Input is guaranteed to be within the range from 1 to 3999.
+ *
+ * Input: 3
+ * Output: "III"
+
+ * Input: 4
+ * Output: "IV"
+
+ * Input: 9
+ * Output: "IX"
+
+ * Input: 58
+ * Output: "LVIII"
+ * Explanation: C = 100, L = 50, XXX = 30 and III = 3.
+ */
+
+/**
+ * @param {number} num
+ * @return {string}
+ */
+const intToRoman = function(num) {
+  let result = "";
+  const decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const roman = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I"
+  ];
+
+  for (let i = 0; i <= decimal.length; i++) {
+    while (num % decimal[i] < num) {
+      result += roman[i];
+      num -= decimal[i];
+    }
+  }
+
+  return result;
+};
+
+const result = intToRoman(3763);
+console.log(result);
